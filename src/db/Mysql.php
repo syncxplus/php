ae79a7e7e7ca61b63b471c36a33264634ecc90de
jibo <jibo@outlook.com>
2019-06-19 11:49:47 +0800

    :bug: Check mysql connection timeout

--- src/db/Mysql.php
+++ src/db/Mysql.php
@@ -4,20 +4,22 @@ namespace db;
 
 class Mysql extends \Prefab
 {
+    const PERIOD_IN_ADVANCE = 300;
     private $db;
+    private $expireTime;
 
     function get()
     {
+        if (time() > $this->expireTime ?? 0) {
+            $f3 = \Base::instance();
+            $this->db = new SQL(
+                $f3->get('MYSQL_DSN'),
+                $f3->get('MYSQL_USER'),
+                $f3->get('MYSQL_PASSWORD')
+            );
+            list($timeout) = $this->db->exec('show variables like ?', ['wait_timeout']);
+            $this->expireTime = time() + $timeout['Value'] - self::PERIOD_IN_ADVANCE;
+        }
         return $this->db;
-    }
-
-    function __construct()
-    {
-        $f3 = \Base::instance();
-        $this->db = new SQL(
-            $f3->get('MYSQL_DSN'),
-            $f3->get('MYSQL_USER'),
-            $f3->get('MYSQL_PASSWORD')
-        );
     }
 }
